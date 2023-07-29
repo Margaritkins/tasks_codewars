02aface0e1820a9a2ceb5fc5eac0757cb6020496
Margarita <te120883ama@gmail.com>
2023-07-29 23:00:26 +0200

    Add static property & method

--- index.js
+++ index.js
@@ -12,18 +12,21 @@ const getSumOfAllElements = (array) => {
 };
 console.log(getSumOfAllElements(array));
 
-
-class Worker{
-  constructor(firstName, lastName,rate, daysWorked = 0){
-this.firstName = firstName;
-this.lastName = lastName;
-this.rate = rate;
-this.daysWorked = daysWorked;
+class Worker {
+  constructor(firstName, lastName, rate, daysWorked = 0) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.rate = rate;
+    this.daysWorked = daysWorked;
   }
-  getSalary(rate,days){
-    return this.rate*this.daysWorked;
+  getSalary(rate, days) {
+    return this.rate * this.daysWorked;
+  }
+  static amountWorkers = 0;
+  static createWorkerTest(){
+    return new Worker(firstName, lastName, rate, daysWorked = 0);
   }
 }
-const worker1 = new Worker('Bruse','Lee',50,20);
+const worker1 = new Worker("Bruse", "Lee", 50, 20);
 console.log(worker1);
-console.log(worker1.getSalary(50,20));
+console.log(worker1.getSalary(50, 20));
