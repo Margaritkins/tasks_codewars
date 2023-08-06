76eaad56ecaa0bd722ee5ff0f56cc01e199c9e7c
Margarita <te120883ama@gmail.com>
2023-08-06 23:41:13 +0200

    Add IteratorQueue task  solution

--- queue.js
+++ queue.js
@@ -1,3 +1,20 @@
+class IteratorQueue {
+  constructor(queue) {
+    this.queue = queue;
+    // this.curentElem = null;
+    this.currentElemIndex = 0;
+  }
+  next() {
+    const currentElem = this.queue[this.currentElemIndex];
+    if (currentElem) {
+      this.currentElemIndex++;}
+    return {
+      value: currentElem,
+      done: !currentElem
+    };
+  }
+}
+
 class Queue {
   constructor(...args) {
     this._head = 0;
@@ -9,8 +26,13 @@ class Queue {
   get size() {
     return this._tail - this._head;
   }
+
+  [Symbol.iterator]() {
+    return new IteratorQueue(this);
+  }
   en(value) {
     this[`${this._tail}`] = value;
+    // console.log(this[`${this._tail}`]);
     this._tail++;
     return this.size;
   }
@@ -24,17 +46,15 @@ class Queue {
     return this[`${this._head}`];
   }
 }
+const queue1 = new Queue("asd", "true", "aaa", "ttt");
 
-const queue = new Queue(4, 5, "tyu");
-// console.log(queue);
-// console.log(queue.en(678));
-// console.log(queue.de());
+console.log(queue1);
 
-//task
+console.log(...queue1);
 
-const queue1 = new Queue("asd", "true", "aaa");
+// console.log(queue1);
 const queue2 = new Queue(4, 5, 6, 2, 5, 8, 5, 8);
-
+const queue = new Queue(4, 5, "tyu");
 const mergeQueue = (queue1, queue2) => {
   const queue3 = new Queue();
   while (queue1.size || queue2.size) {
@@ -48,4 +68,15 @@ const mergeQueue = (queue1, queue2) => {
   return queue3;
 };
 
-console.log(mergeQueue(queue1, queue2));
+// console.log(queue1.de());
+
+// for (const item of queue1) {
+//   console.log(item);
+// }
+
+// console.log(...queue2);
+// console.log(itereatedQueue(queue1));
+// console.log(queue1.de());
+// console.log(queue1.de());
+// console.log(queue1.de());
+// console.log(mergeQueue(queue1, queue2));
